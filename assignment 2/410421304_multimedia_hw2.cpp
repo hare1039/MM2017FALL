da5faa0ad543656892011c75de6050b481768763
ndhu410421304 <410421304@gms.ndhu.edu.tw>
2017-12-02 23:46:34 +0800

    Import wav format
    import wav format

--- assignment 2/410421304_multimedia_hw2.cpp
+++ assignment 2/410421304_multimedia_hw2.cpp
@@ -15,10 +15,28 @@ const int bit = 16;
 const int amp = 32767; //16 bit: 32767~-32768
 const int buff = sec*Fs*channel;
 
+template<typename Type> void write(ofstream& out, const Type& sting)
+{
+	out.write((const char*)&sting, sizeof(Type));
+}
+
 int main(int argc, char** argv)
 {
     ofstream out;
     out.open("sintest.wav", ios::binary);
+
+    out.write("RIFF", 4);
+    write<int>(out, 36 + (buff*sizeof(signed short)));
+    out.write("WAVEfmt ", 8);
+    write<int>(out, 16);
+    write<short>(out, 1);
+    write<unsigned short>(out, channel);
+    write<int>(out, Fs);
+    write<int>(out, Fs*channel*sizeof(signed short));
+    write<short>(out, channel*sizeof(signed short));
+    write<short>(out, sizeof(signed short)*8);
+    out.write("data", 4);
+    write<int>(out, buff*sizeof(signed short));
 
     double siz[buff];
     memset(siz, 0 , buff*2); //set memory space for siz
