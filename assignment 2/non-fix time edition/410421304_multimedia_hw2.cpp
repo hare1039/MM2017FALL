4ec6de14cfb8062c80c7e9bd4cad14a2aca80f95
ndhu410421304 <410421304@gms.ndhu.edu.tw>
2017-12-06 21:13:13 +0800

    Testing for a non-fix time edition
    Tested version added

---
+++ assignment 2/non-fix time edition/410421304_multimedia_hw2.cpp
@@ -0,0 +1,303 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdio.h>
+#include <string.h>
+#include <cmath>
+
+using namespace std;
+
+//initial part
+const int freq = 400;
+const double Fs = 8000; //sampler rate
+int sec = 5;
+const int channel = 1;
+const int bit = 16;
+const int amp = 32767; //16 bit: 32767~-32768
+int buff = sec*Fs*channel;
+const double D = 1;
+
+template<typename Type> void write(ofstream& out, const Type& sting)
+{
+	out.write((const char*)&sting, sizeof(Type));
+}
+
+int main(int argc, char** argv)
+{
+    //input part
+    char notes[1000]; // music note in char form
+    char singlenote;
+
+    ifstream imnotes;
+    imnotes.open("music note.txt");
+
+    int counter = 0;
+    double metre =0;
+
+    if(imnotes.is_open())
+    {
+        while(!imnotes.eof())
+        {
+            if(sec == 5 && metre == 0)
+            {
+                imnotes>>sec;
+                buff = sec*Fs*channel;
+                imnotes>>metre;
+            }
+            else
+            {
+                imnotes>>singlenote;
+                notes[counter] = singlenote;
+                counter = counter+1;
+                cout<<"1";
+            }
+        }
+    }
+
+
+    //process with input notes
+
+    counter = 0;
+    int counter2 = 0;
+    int intnotes[1000]; //array to set notes in decimal form
+
+    while(counter!=1000)
+    {
+        if((notes[counter] >= 48) && (notes[counter] <= 57))
+        {
+            intnotes[counter2] = notes[counter] - 48;
+            counter2++;
+        }
+        counter++;
+    }
+
+    int counter3 = 0;
+
+    double level2[1000];
+    double level5[1000];
+
+    while(counter3 < (counter2-1))
+    {
+        if(intnotes[counter3] == 0)
+        {
+            level2[counter3] = 1;
+            level5[counter3] = 1;
+        }
+        else if(intnotes[counter3] == 1)
+        {
+            level2[counter3] = 65;
+            level5[counter3] = 523;
+        }
+        else if(intnotes[counter3] == 2)
+        {
+            level2[counter3] = 73;
+            level5[counter3] = 587;
+        }
+        else if(intnotes[counter3] == 3)
+        {
+            level2[counter3] = 82;
+            level5[counter3] = 659;
+        }
+        else if(intnotes[counter3] == 4)
+        {
+            level2[counter3] = 87;
+            level5[counter3] = 699;
+        }
+        else if(intnotes[counter3] == 5)
+        {
+            level2[counter3] = 98;
+            level5[counter3] = 784;
+        }
+        else if(intnotes[counter3] == 6)
+        {
+            level2[counter3] = 110;
+            level5[counter3] = 880;
+        }
+        else if(intnotes[counter3] == 7)
+        {
+            level2[counter3] = 124;
+            level5[counter3] = 988;
+        }
+        counter3++;
+    }
+
+    //wav format setting
+    ofstream out;
+    out.open("normalmerge.wav", ios::binary);
+
+    out.write("RIFF", 4);
+    write<int>(out, 36 + (buff*sizeof(signed short)));
+    out.write("WAVEfmt ", 8);
+    write<int>(out, 16);
+    write<short>(out, 1);
+    write<unsigned short>(out, channel);
+    write<int>(out, Fs);
+    write<int>(out, Fs*channel*sizeof(signed short));
+    write<short>(out, channel*sizeof(signed short));
+    write<short>(out, sizeof(signed short)*8);
+    out.write("data", 4);
+    write<int>(out, buff*sizeof(signed short));
+
+    //for part2 f = 100
+    ofstream out100;
+    out100.open("cosf100.wav", ios::binary);
+
+    out100.write("RIFF", 4);
+    write<int>(out100, 36 + (buff*sizeof(signed short)));
+    out100.write("WAVEfmt ", 8);
+    write<int>(out100, 16);
+    write<short>(out100, 1);
+    write<unsigned short>(out100, channel);
+    write<int>(out100, Fs);
+    write<int>(out100, Fs*channel*sizeof(signed short));
+    write<short>(out100, channel*sizeof(signed short));
+    write<short>(out100, sizeof(signed short)*8);
+    out100.write("data", 4);
+    write<int>(out100, buff*sizeof(signed short));
+
+    //for part2 f = 500
+    ofstream out500;
+    out500.open("cosf500.wav", ios::binary);
+
+    out500.write("RIFF", 4);
+    write<int>(out500, 36 + (buff*sizeof(signed short)));
+    out500.write("WAVEfmt ", 8);
+    write<int>(out500, 16);
+    write<short>(out500, 1);
+    write<unsigned short>(out500, channel);
+    write<int>(out500, Fs);
+    write<int>(out500, Fs*channel*sizeof(signed short));
+    write<short>(out500, channel*sizeof(signed short));
+    write<short>(out500, sizeof(signed short)*8);
+    out500.write("data", 4);
+    write<int>(out500, buff*sizeof(signed short));
+
+    //for part2 f = 800
+    ofstream out800;
+    out800.open("cosf800.wav", ios::binary);
+
+    out800.write("RIFF", 4);
+    write<int>(out800, 36 + (buff*sizeof(signed short)));
+    out800.write("WAVEfmt ", 8);
+    write<int>(out800, 16);
+    write<short>(out800, 1);
+    write<unsigned short>(out800, channel);
+    write<int>(out800, Fs);
+    write<int>(out800, Fs*channel*sizeof(signed short));
+    write<short>(out800, channel*sizeof(signed short));
+    write<short>(out800, sizeof(signed short)*8);
+    out800.write("data", 4);
+    write<int>(out800, buff*sizeof(signed short));
+
+
+    //arithmetic part
+
+    //for mix
+    short siz[buff];
+    //for levels
+    short siz2[buff];
+    short siz5[buff];
+
+    memset(siz, 0 , buff*2); //set memory space for siz
+
+
+    int i = 0;
+    counter = 0;
+    double doublecounter = 0;
+
+    for(i=0; i<buff; i++)
+    {
+        double checker = (i/Fs);
+        if(checker < 1)
+        siz2[i] = sin((2 * 3.14)* 1 * i / Fs )*amp;
+        else
+        {
+            siz2[i] = sin((2 * 3.14)* level5[counter] * i / Fs )*amp; // formula in pdf
+            doublecounter+=(1/Fs);
+            if(doublecounter >= metre)
+            {
+                counter+=1;
+                doublecounter=0;
+            }
+        }
+    }
+
+    counter = 0;
+    doublecounter = 0;
+
+    for(i=0; i<buff; i++)
+    {
+        double checker = (i/Fs);
+        if((sec-checker) <= 1)
+        siz5[i] = sin((2 * 3.14)* 1 * i / Fs )*amp;
+        else
+        {
+            siz5[i] = sin((2 * 3.14)* level2[counter] * i / Fs )*amp; // formula in pdf
+            doublecounter+=(1/Fs);
+            if(doublecounter >= metre)
+            {
+                counter+=1;
+                doublecounter=0;
+            }
+        }
+    }
+
+    //output part
+    counter = 0;
+
+    for(int i=0; i<buff; i++)
+    {
+        siz[i] = (int)(0.5*siz2[i] + 0.5*siz5[i]);
+
+        if(siz[i] > 32767)
+            siz[i] = 32767;
+        else if(siz[i] < -32768)
+            siz[i] = -32768;
+    }
+
+    out.write((const char*)&siz[0], buff*2);
+
+    //100 test
+    for(int i=0; i<buff; i++)
+    {
+        siz[i] = (int)(0.5*siz2[i]*cos((2 * 3.14)* 100 * i / Fs ) + 0.5*siz5[i]*cos((2 * 3.14)* 100 * i / Fs ));
+
+        if(siz[i] > 32767)
+            siz[i] = 32767;
+        else if(siz[i] < -32768)
+            siz[i] = -32768;
+    }
+
+    out100.write((const char*)&siz[0], buff*2);
+
+    //500 test
+    for(int i=0; i<buff; i++)
+    {
+        siz[i] = (int)(0.5*siz2[i]*cos((2 * 3.14)* 500 * i / Fs ) + 0.5*siz5[i]*cos((2 * 3.14)* 500 * i / Fs ));
+
+        if(siz[i] > 32767)
+            siz[i] = 32767;
+        else if(siz[i] < -32768)
+            siz[i] = -32768;
+    }
+
+    out500.write((const char*)&siz[0], buff*2);
+
+    //800 test
+    for(int i=0; i<buff; i++)
+    {
+        siz[i] = (int)(0.5*siz2[i]*cos((2 * 3.14)* 800 * i / Fs ) + 0.5*siz5[i]*cos((2 * 3.14)* 800 * i / Fs ));
+
+        if(siz[i] > 32767)
+            siz[i] = 32767;
+        else if(siz[i] < -32768)
+            siz[i] = -32768;
+    }
+
+    out800.write((const char*)&siz[0], buff*2);
+
+    cout<<"Output complete.";
+
+    out.close();
+    return 0;
+}
---
+++ assignment 2/non-fix time edition/README.md
@@ -0,0 +1,103 @@
+# Mutimedia Assignment 2
+
+# Report and Discussion
+About the format:
+* Example:
+
+  0.6
+
+  5 3 3 0 4 2 2 0
+
+  1 2 3 4 5 5 2 1
+
+The first input is use as the **metre** in the program, which represent how much time each music note will take.
+In the given exampl it would be 0.6. And the music note were transfer to decimal in following format:
+C = 1 D = 2 E = 3 F = 4 G = 5 A = 6 B = 7. Rest note would be 0.
+By this order, we could know that the example is song "little bee" with a slight modified.
+When the total time exceed the space wav file had been given, it would stop recording.
+ 
+Flowchart:
+![pic]( https://user-images.githubusercontent.com/33059129/33530090-ff2e9ce6-d8b4-11e7-988f-140c44e02b4e.PNG "flow chart")
+About how my program works, it was made up with serveral parts:
+* First part is initial part. The values such as Fs, frquncy, sec, etc. With all these values here, we could easliy make
+  change or modified the program just by do change in here.
+* Second part is the input part. Contain with code dealing with input datas and change them into the information we want, such as 
+  change music notes into frequncy.
+* Third part is the wav file setting parts. In here, we set up the required format for the wav files we are going to generate
+  by the values we had initial from 1st part. This part include the function we define above.
+* 4th part is the arithmeitc part. What we do is following the formula we had been given from the pdf and rules to create the 
+  required data for wav file by using the frequncies from part 2 and values from part 1.
+* The last part is the output part. Use the data we had get from 4th part to put int the wav file we had set up in part3, then
+  its all done!
+   
+How to run my program:
+
+Its simple! What you need to do is simply set up your music note in music note.txt, run the 410421304_multimedia_hw2.exe for execute
+or run .cpp by compiler, then the result of normal merge would be normalmerge.wav, modulation with f=100 would be cosf100.wav, with
+f=500 would be cosf500.wav, and f=800 would be cosf800.wav. And thats all of the requied results!
+
+Result display:
+
+Use some external program (here I use OpenMPT) to generate the visual shape of the wave file.
+
+* Normal Merge (normal merge.wav) :
+![pic]( https://user-images.githubusercontent.com/33059129/33530100-3c3c059c-d8b5-11e7-9166-5a388a56c4b3.PNG "normal merge")
+ 
+ We can see that two wave merge to one with a time delay.
+ 
+ * Modulation with f = 100 (cosf100.wav):
+![pic]( https://user-images.githubusercontent.com/33059129/33530120-5e92b3b6-d8b5-11e7-9248-cc6e74d42e5e.PNG "100")
+* Modulation with f = 500 (cosf500.wav):
+![pic]( https://user-images.githubusercontent.com/33059129/33530126-7daafb32-d8b5-11e7-96b2-65b3cee715b4.PNG "500")
+* Modulation with f = 800 (cosf800.wav):
+![pic]( https://user-images.githubusercontent.com/33059129/33530128-8bfb5254-d8b5-11e7-8430-aee6737f4946.PNG "800")
+
+It seems to be some frequncy change before and after modulation, but after modulation it seems to be at same frequncy on f change.
+
+Note: You can find these wav files under assignment 2's directory. If the web audio player does not work, try to download the file.)
+
+Problem facing:
+
+Countless problem face in making this program, just list some important parts.
+Of course, the first thing is to set up the wav file format. Without using library, I decide to do this by myself. But there are a
+lot of things to set in wav file, so I trying to set up program with simple sin wave first. At first, the file even not generate. Then
+it generate, but the file was crashed, due to the wrong formating. Check through numerous page taliking about wav file and countless of
+try hards, finally it was done. Mostly done by try and error.
+
+At by this build-up settings, something that I did not notice and accept by the system but actually wrong finally would be solve during the run-through test outputs, although some problem was finally been found and some thing was finally been notice at the very end of this programing. A lot of things is "learned" by program error, such as how to set up the type "correctly" to make program correct--not only for the single launch up.
+
+That is the main problem, other than that the second important thing is how to merge 2 programs. I found someone said just "add them up", so I simply add them up, and result with head-blowing noise. Then I know it should be "add them by rates".
+
+Input file is a small thing, though I nearly forget this basic, so take some time re-study this basic.
+
+And also some small but annoying problem, such as output always level2 or level5 sound, then I need to walk back to check where is wrong; program cannot wrong with propriate header; or even the compiler setting...
+
+Its good thing they all be conquired by try hards.
+
+Result discussion:
+
+* As the result display above and the wav file we got:
+  * Level intefere the frequncy's hi-lo, low level low and high level high.
+  * Not nuch thing to talk about merging. Just simply set up the time delay for each level. Basso(low) part come out first, so it
+    may discard some part at the end; on the contrary, alto(high) part would abandon same amount from the begining.
+  * The modulation part, by the result we could found that with greater frequncy, the sound would become more sharp and thick, and
+    the frequncy of the result surely change by add this modified.
+
+What I have learn:
+
+I learn a lot of things from this assignment. Such as how to set up wav files for generates sounds; how to input files; how to use 
+sin/cos formula with header cmath; file stream with type aside text; how to generate song with simple music notes; how to change the
+pitch tone between levles; and how to merge two wav file into one.
+
+Aside hard or not, this assignment really torture to hear those "wrong" and noisy sound for hours though.
+
+Talking about some feeling during and after the programing. It have so much little troubles intefere me to complete this assignment.
+I was doing type correction really often thanks to wrong set up at the begining. By all these I keep asking some "small" question to
+my former classmate, like "Why my double won't show float value?" and reason is simple:int delete int. And I make this mistake twice.
+Type correction, I set up buffer by interger at the begining. It then change to double, change back to int, to float, till I finally
+realize it was not a short for no reason, then it finally been set up as short. But the thing is that I also gain some knowledge and
+found some interesting plot inside this, such as by changeing type, the actual output voice would also be different, and some settings must be change either; or some wrong buff size would change the length of the voice; finally realize I actually output a level 5 instead of level 2 until I start to merging them, even some basic things like 8bit=1byte, I knew that, but when I finally understand why all these settings are 2, 4, 8... it was like when Archimedes shouting out "Eureka!" though I did not really found something important, but thats something imporatnt to those someone who had some hard time in programing, this may be a small but important push to make them keep going. Although it have hard time, but spring always come after winter, and it will be all well paid for this.
+
+
+*Readding, coding, debugging. This is what I had done this program, by this circulation.*
+  
